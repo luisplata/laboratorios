12db4539c9d4475a7c6323515686f4240c964b76
Luis Plata <www.luisplata@gmail.com>
2020-06-01 23:59:26 -0500

    vamos a crear juegos para algebra lineal

---
+++ Assets/Algebra.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Algebra : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //vamos a mover el objeto a la velocidad de la operacion
+    float x;
+    public int valorX;
+    public TextMeshProUGUI texto;
+    void Start()
+    {
+        //operacion 3x+4x^2
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = 3 * valorX + 4 * valorX ^ 2;
+        texto.text = x.ToString();
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x,0);
+    }
+}
--- Assets/pacman_laboratorio/v2/CreadorDeCuadrilla.cs
+++ Assets/pacman_laboratorio/v2/CreadorDeCuadrilla.cs
@@ -14,9 +14,11 @@ public class CreadorDeCuadrilla : MonoBehaviour {
         //float distancia = cuadrado.transform.localScale.x;
         //creamos la cuadrilla solamente en el start y hacemos el for para eso
         Vector3 posicionInicial = transform.position;
-        for (int i = 0; i < m; i++) {
-            for (int o = 0; o < n; o++) {
-                listaDeCuadrillaCreada.Add (Instantiate (cuadrado, transform.position, transform.rotation));
+        for (int i = 1; i <= m; i++) {
+            for (int o = 1; o <= n; o++) {
+                GameObject instancia = Instantiate(cuadrado, transform.position, transform.rotation);
+                instancia.name = i + "_" + o;
+                listaDeCuadrillaCreada.Add (instancia);
                 Vector3 nuevaPosicion = transform.position + new Vector3 (distancia, 0, 0);
                 transform.position = nuevaPosicion;
             }
