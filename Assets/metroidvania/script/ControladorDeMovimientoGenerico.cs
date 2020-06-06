838a00134b2cb7752cbd69660d59d219cb5e1050
Luis Plata <www.luisplata@gmail.com>
2020-06-06 00:34:25 -0500

    agregando sprites para un metroidvania

---
+++ Assets/metroidvania/script/ControladorDeMovimientoGenerico.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDeMovimientoGenerico : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+    }
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * speed);
+    }
+}
