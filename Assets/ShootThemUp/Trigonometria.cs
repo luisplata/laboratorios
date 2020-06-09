c4f8d557847bc35383172a424f93e61828680790
Luis Plata <www.luisplata@gmail.com>
2020-06-08 23:03:08 -0500

    primera prueba de movimiento de enemigo!

---
+++ Assets/ShootThemUp/Trigonometria.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigonometria : MonoBehaviour
+{
+    //vamos a mostrar primero resultados
+    public float teta;
+    //ahora vamos a darle movimiento a l objeto
+    public GameObject objeto;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //teta es x and y es el seno
+        objeto.transform.position = new Vector2(teta, Mathf.Sin(teta));
+        Debug.Log(Mathf.Sin(teta));
+        teta += Time.deltaTime;
+    }
+}
